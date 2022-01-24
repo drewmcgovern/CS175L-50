1e36961988c1bec9ac2f6dcb094dd2f6264653f7
Andrew McGovern <91150777+drewmcgovern@users.noreply.github.com>
2022-01-24 18:53:17 -0500

    myinfo version 2

--- myinfo.py
+++ myinfo.py
@@ -3,4 +3,6 @@
 #myInfo
 
 print("My name is Andrew McGovern")
-print("I am a student")
+print("I live at 431 Cedar Ave, West Long Branch, New Jersey, 07764")
+print("My telephone number is 123-456-7890")
+print("My major is Software Engineering")
